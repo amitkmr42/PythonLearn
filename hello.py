6601ead91e196f43cff670afa96d761f74c0ba19
amitkmr42 <amitkmr42@gmail.com>
2026-09-20 14:48:42 +0530

    made changes

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World")
+# Ask user for their name
+name = input("What's your name? ")
+
+# Say hello to user
+
+"""
+This is also a comment
+But this is a multi line comment
+"""
+print("Hello, " + name)
