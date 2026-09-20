dfcaeb88af663dfc4b7232a8dbc1f627a180d3a1
amitkmr42 <amitkmr42@gmail.com>
2026-09-20 17:47:36 +0530

    Pause1 20/09/26

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 # Ask user for their name
 name = input("What's your name? ")
 
+# Remove whitespace from str and capitalize user's name
+name=name.strip().title()
+
 # Say hello to user
 
 """
 This is also a comment
 But this is a multi line comment
 """
-print("Hello, " + name)
+print(f"Hello, {name}")
+
+# Delete this. This is a marker. Harvard CS50 Python. Time paused 57:04 from 15:57:48
