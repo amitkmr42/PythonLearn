e0075517d661355d62f208cc3d19a5e818664a7b
amitkmr42 <amitkmr42@gmail.com>
2026-09-21 19:16:58 +0530

    R3 210926

---
+++ Python by CS50/Variables & Def/calculator.py
@@ -0,0 +1,10 @@
+def main():
+    x=int(input("What is x? "))
+    print("x squared is ", square(x))
+
+
+def square(n):
+    return pow(n,2)
+
+main()
+# Delete this. This is a marker. Harvard CS50 Python. Time paused 57:04 from 15:57:48
--- calculator.py
+++
@@ -1,9 +0,0 @@
-x=float(input("What's x? "))
-y=float(input("What's y? "))
-
-z=x/y
-
-
-print(f"{z:.2f}")
-
-# Delete this. This is a marker. Harvard CS50 Python. Time paused 57:04 from 15:57:48
