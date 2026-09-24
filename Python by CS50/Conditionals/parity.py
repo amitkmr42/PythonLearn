6e235e0724f8feaff5359d6f67255b50c8fd90b7
amitkmr42 <amitkmr42@gmail.com>
2026-09-24 16:04:26 +0530

    R1 240926

--- Python by CS50/Conditionals/parity.py
+++ Python by CS50/Conditionals/parity.py
@@ -1,6 +1,12 @@
-x= int(input("What's x? "))
+def main():
+    x= int(input("What's x? "))
 
-if x%2 == 0:
-    print("Even")
-else:
-    print("odd")
+    if is_even (x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    return n%2 ==0
+
+main()
